3a02f7dbf36199dfedb28b98ac941084a88d0055
Marcel Cutts <me@marcelcutts.com>
2014-07-29 18:41:10 +0100

    Added comments for server description (and testing control flow of dev box)

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 var restify = require('restify');
 
-// Dev settings
+/**
+ * Dev server settings
+ * @type {String}
+ */
 var port = '8080';
 var ipAddress = '127.0.0.1';
 
+/**
+ * Create, name and version server object
+ */
 var server = restify.createServer({
 	name: "yo-yo",
 	version: '0.0.1'
 });
 
+/**
+ * Placeholder text to return some content at root
+ * @param   req  Properties of the HTTP request
+ * @param   res  Properties of the HTTP result
+ * @param   next Async flow control object
+ * @return next object
+ */
 server.get('/', function(req, res, next) {
 	res.writeHead(200, {
 		'Content-Type': 'application/json; charset=utf-8'
@@ -17,6 +30,9 @@ server.get('/', function(req, res, next) {
 	return next();
 });
 
+/**
+ * Initialises server at a certain location and informs user
+ */
 server.listen(port, ipAddress, function() {
 	console.log('%s listening at %s ', server.name, server.url);
 });
